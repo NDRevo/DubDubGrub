83c7e0d3e10defcf35d9bdf9f60222cc001414bd
Noe Duran <durann031@gmail.com>
2022-01-02 11:49:31 -0500

    Added Photopicker: Can add photo from photo library and edit it. Once picked, can dismiss. Built with UIViewControllerRepresentable since SwiftUI doesn't have photo picker as of iOS 14

---
+++ DubDubGrub/Views/CustomViews/UIKit Components/PhotoPicker.swift
@@ -0,0 +1,44 @@
+//
+//  PhotoPicker.swift
+//  DubDubGrub
+//
+//  Created by Noé Duran on 1/2/22.
+//
+
+import SwiftUI
+
+struct PhotoPicker: UIViewControllerRepresentable {
+    
+    @Binding var image: UIImage
+    @Environment(\.presentationMode) var presentationMode
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+    
+    final class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let photoPicker: PhotoPicker
+        
+        init(photoPicker: PhotoPicker){
+            self.photoPicker = photoPicker
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[.editedImage] as? UIImage {
+                let compressedImageData = image.jpegData(compressionQuality: 0.1)!
+                photoPicker.image = UIImage(data: compressedImageData)!
+            }
+            
+            photoPicker.presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(photoPicker: self)
+    }
+}
